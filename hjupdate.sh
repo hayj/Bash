eeb4423a03a5240c86bfbd4d259e1dfbe9b59f31
hayj <hay.julien1@gmail.com>
2018-04-21 17:14:47 +0200

    minor updates

--- hjupdate.sh
+++ hjupdate.sh
@@ -1,5 +1,11 @@
 #!/bin/bash - 
 
+# To use this script:
+# 1. add it in "~/.local/bin/" as "hjupdate"
+# 2. do "chmod +x ~/.local/bin/hjupdate"
+# 3. execute it using "hjupdate" (will install main hj packages)
+# 4. execute it using "hjupdate -a" (will ALL hj packages)
+
 # Global vars:
 before="==> "
 after=" <=="
