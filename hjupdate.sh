eba0c501b8b5b182e8f6728ccae79a643584d1f4
hayj <hay.julien1@gmail.com>
2018-04-24 21:05:39 +0200

    minor updates

--- hjupdate.sh
+++ hjupdate.sh
@@ -13,82 +13,82 @@ after=" <=="
 installAll=0
 packageToInstall=
 while test $# -gt 0; do
-	case "$1" in
-		-h|--help)
-			echo "Install hj python packages, by default only main pakages"
-			echo "options:"
-			echo "-a, --all	Install all available packages (instead of the minimal)"
-			echo "-p, --package	Install only the specified package"
-			exit 0
-			;;
-		-a|--all)
-			installAll=1
-			shift
-			;;
-		-p|--package)
-			shift
-			packageToInstall=$1
-			shift
-			;;
-		*)
-			break
-			;;
-	esac
+    case "$1" in
+        -h|--help)
+            echo "Install hj python packages, by default only main pakages"
+            echo "options:"
+            echo "-a, --all    Install all available packages (instead of the minimal)"
+            echo "-p, --package    Install only the specified package"
+            exit 0
+            ;;
+        -a|--all)
+            installAll=1
+            shift
+            ;;
+        -p|--package)
+            shift
+            packageToInstall=$1
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
 done
 
 
 packageToInstallLength=${#packageToInstall}
 if [[ $packageToInstallLength > 0 ]]; then
-	installAll=1
+    installAll=1
 fi
 
 isToInstall()
 {
-	if [[ $packageToInstallLength > 0 ]]; then
-		if [ "$1" == "$packageToInstall" ]; then
-			echo 1
-		else
-			echo 0
-		fi
-	else
-		echo 1
-	fi
+    if [[ $packageToInstallLength > 0 ]]; then
+        if [ "$1" == "$packageToInstall" ]; then
+            echo 1
+        else
+            echo 0
+        fi
+    else
+        echo 1
+    fi
 }
 
 # Installing some apt-get packages:
-# jq git htop rsync pandoc tree unzip p7zip-full
+# jq git htop rsync pandoc tree unzip p7zip-full vim python-pip tk
 aptUpdateDone=0
 for current in git htop rsync ; do
-	commandResult=$(command -v $current)
-	isInstalled=$(echo -n $commandResult | wc -c)
-	if [[ $isInstalled = 0 ]]; then
-		if [[ $aptUpdateDone = 0 ]]; then
-			echo "Updating apt-get..."
-			sudo apt-get update
-			aptUpdateDone=1
-		fi
-		echo "$current will be installed..."
-	  	sudo apt-get -y install $current
-	else
-		echo "$current is already installed."
-	fi
+    commandResult=$(command -v $current)
+    isInstalled=$(echo -n $commandResult | wc -c)
+    if [[ $isInstalled = 0 ]]; then
+        if [[ $aptUpdateDone = 0 ]]; then
+            echo "Updating apt-get..."
+            sudo apt-get update
+            aptUpdateDone=1
+        fi
+        echo "$current will be installed..."
+          sudo apt-get -y install $current
+    else
+        echo "$current is already installed."
+    fi
 done
 
 # Installing some pip packages:
 # for current in pew workspacemanager ; do
-# 	if [[ $(sudo pip freeze | grep $current) != *"$current=="* ]]; then
-# 		echo "$current will be installed..."
-# 		sudo pip install $current
-# 	else
-# 		echo "$current is already installed."
-# 	fi
+#     if [[ $(sudo pip freeze | grep $current) != *"$current=="* ]]; then
+#         echo "$current will be installed..."
+#         sudo pip install $current
+#     else
+#         echo "$current is already installed."
+#     fi
 # done
 
 # Print param infos:
 if [[ $installAll = 0 ]]; then
-	echo $before"Installing hj main packages..."$after
+    echo $before"Installing hj main packages..."$after
 else
-	echo $before"Installing all hj packages..."$after
+    echo $before"Installing all hj packages..."$after
 fi  
 
 # Creating a tmp directory:
@@ -102,34 +102,34 @@ mkdir -p $tmpDir
 # We create the install funct:
 installGz()
 {
-	package=$1
-	gzPattern=$1
-	if [ $(isToInstall "$package") == "1" ]; then
-		echo $before"Installing $package..."$after
-		if [[ $package = *"hj"* ]]; then
-			gzPattern=${gzPattern:2}
-		fi
-		gzPattern=*$gzPattern*.tar.gz
-		pip uninstall -y $package
-		pip install $webcrawlerWmdistPath/$gzPattern
-	fi
+    package=$1
+    gzPattern=$1
+    if [ $(isToInstall "$package") == "1" ]; then
+        echo $before"Installing $package..."$after
+        if [[ $package = *"hj"* ]]; then
+            gzPattern=${gzPattern:2}
+        fi
+        gzPattern=*$gzPattern*.tar.gz
+        pip uninstall -y $package
+        pip install $webcrawlerWmdistPath/$gzPattern
+    fi
 }
 
 # We create a funct to insall from github:
 installFromGithub()
 {
-	currentDir=$(pwd)
-	projectName=$1
-	package=$(echo $projectName | tr '[:upper:]' '[:lower:]')
-	if [ $(isToInstall "$package") == "1" ]; then
-		echo $before"Installing $package..."$after
-		packagePath=$tmpDir/$projectName
-		git clone -q https://github.com/hayj/$projectName.git $packagePath
-		pip uninstall -y $package
-		cd $packagePath
-		python setup.py install
-		cd $currentDir
-	fi
+    currentDir=$(pwd)
+    projectName=$1
+    package=$(echo $projectName | tr '[:upper:]' '[:lower:]')
+    if [ $(isToInstall "$package") == "1" ]; then
+        echo $before"Installing $package..."$after
+        packagePath=$tmpDir/$projectName
+        git clone -q https://github.com/hayj/$projectName.git $packagePath
+        pip uninstall -y $package
+        cd $packagePath
+        python setup.py install
+        cd $currentDir
+    fi
 }
 
 # We download packages in tar.gz:
@@ -139,18 +139,18 @@ git clone -q https://github.com/hayj/WebCrawler.git $webcrawlerPath
 
 # For all main packages:
 for i in "hjsystemtools" "datastructuretools" "databasetools" "datatools"; do
-	installGz $i
+    installGz $i
 done
 
 # For all others packages:
 if [[ $installAll = 1 ]]; then
-	for i in "domainduplicate" "error404detector" "honeypotdetector" "machinelearning" "nlptools" "unshortener" "webbrowser" "webcrawler"; do
-		installGz $i
-	done
-	# Other packages:
-	for i in "NewsTools" "Scroller"; do
-		installFromGithub $i
-	done
+    for i in "domainduplicate" "error404detector" "honeypotdetector" "machinelearning" "nlptools" "unshortener" "webbrowser" "webcrawler"; do
+        installGz $i
+    done
+    # Other packages:
+    for i in "NewsTools" "Scroller"; do
+        installFromGithub $i
+    done
 fi
 
 # Removing the tmp dir:
